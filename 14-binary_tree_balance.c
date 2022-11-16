2d7955ccbef9cae77d0fd4c313526991d4e1a787
apha-pesso <mutiuayo99@gmail.com>
2022-11-16 12:19:03 +0000

    Add function to get heigh

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,21 +9,21 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (tree)
-        {
-                size_t l = 0, r = 0;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-        if (tree->left)
-        {
-                l = 1 + binary_tree_height(tree->left);
-        }
-        else
-                l = 0;
+	if (tree->left)
+	{
+		l = 1 + binary_tree_height(tree->left);
+	}
+	else
+		l = 0;
 
-        r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-        return ((l > r) ? l : r);
-        }
-        return (0);
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((l > r) ? l : r);
+	}
+	return (0);
 }
 
 
