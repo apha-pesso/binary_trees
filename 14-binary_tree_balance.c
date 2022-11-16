603a0d22c02fbb8db4899871d3a85477805f44f1
apha-pesso <mutiuayo99@gmail.com>
2022-11-16 12:15:52 +0000

    Add function to get heigh

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,32 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_height- Compute the height of a binary tree
+ * @tree: pointer to the root node
+ * Return: return height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        if (tree)
+        {
+                size_t l = 0, r = 0;
+
+        if (tree->left)
+        {
+                l = 1 + binary_tree_height(tree->left);
+        }
+        else
+                l = 0;
+
+        r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+        return ((l > r) ? l : r);
+        }
+        return (0);
+}
+
+
 /**
  * binary_tree_balance- measures the balance factor of a binary tree
  * @tree: Pointer to the root node
