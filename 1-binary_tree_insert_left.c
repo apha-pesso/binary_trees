08a027b0b4a7287d5e5cbdb49472da4dcab35b91
apha-pesso <mutiuayo99@gmail.com>
2022-11-18 07:14:49 +0000

    edge case for when parent is NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new = malloc(sizeof(binary_tree_t));
 
-
+	if (parent == NULL)
+		return (NULL);
 	if (new == NULL)
 		return (NULL);
 	new->left = NULL;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
+	if (parent == NULL)
+		return (NULL);
 	new->left = NULL;
 	new->right = NULL;
 	new->n = value;
